d8ad4b5624022eddc423b78525da96a5752d42ea
Nikhil <nkrai774@gmail.com>
2024-01-17 23:07:07 +0530

    Update q10.java

--- Assignment_06/q10.java
+++ Assignment_06/q10.java
@@ -3,13 +3,25 @@ public class q10 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the expression: ");
-        String expression = scanner.nextLine();
+        String exp = scanner.nextLine();
         scanner.close();
-        String[] parts = expression.split(" ");
-        int operand1 = Integer.parseInt(parts[0]);
-        int operand2 = Integer.parseInt(parts[2]);
-        char operator = parts[1].charAt(0);
-        int result = 0;
+        char operator=' ';
+        int operand1=0,operand2=0;
+        int i=0;
+        while (i<exp.length()) {
+            if (exp.charAt(i) >= '0' && exp.charAt(i) <= '9') {
+                if (operator == ' ') {
+                    operand1 =operand1*10+Character.getNumericValue(exp.charAt(i));
+                } else {
+                    operand2 =operand2*10+Character.getNumericValue(exp.charAt(i));
+                }
+            }
+            else{
+                operator=exp.charAt(i);
+            }
+            i++;
+        }
+        int result=0;
         switch (operator) {
             case '+':
                 result = operand1 + operand2;
